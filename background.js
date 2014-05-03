f7cad482f37df789fece127e6303a3dd6e851e40
Simeon Mugisha <cmeo226@yahoo.com>
2014-05-03 23:46:38 +0800

    window size is fixed and added logo image placeholder

--- background.js
+++ background.js
@@ -5,13 +5,17 @@ chrome.app.runtime.onLaunched.addListener( function() {
 
   var width = 300;
   var height = 300;
-
   chrome.app.window.create('login.html', {
     'bounds': {
       width: width,
       height: height,
       left: Math.round((screen.availWidth-width)/2),
       top: Math.round((screen.availHeight-height)/2)
-    }
+    },
+    minWidth: width,
+    minHeight: height,
+    maxWidth: width,
+    maxHeight: height
   });
+
 });
