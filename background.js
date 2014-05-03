56c58b89cc1720186a604dc1bee297c52d4f69c2
Simeon Mugisha <cmeo226@yahoo.com>
2014-05-03 23:09:19 +0800

    window loads in the center even when maximized, app still in center

--- background.js
+++ background.js
@@ -2,10 +2,16 @@
 // Developed by Simeon Mugisha Rwegayura
 
 chrome.app.runtime.onLaunched.addListener( function() {
+
+  var width = 300;
+  var height = 300;
+
   chrome.app.window.create('login.html', {
     'bounds': {
-      'width': 300,
-      'height': 300
+      width: width,
+      height: height,
+      left: Math.round((screen.availWidth-width)/2),
+      top: Math.round((screen.availHeight-height)/2)
     }
   });
 });
