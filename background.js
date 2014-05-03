754b761edbd51c783f8ecb7b4eacf0655fc931c1
Simeon Mugisha <cmeo226@yahoo.com>
2014-05-03 19:21:13 +0800

    corrected weight error to width

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 // Auto Wifi MMU login app
 // Developed by Simeon Mugisha Rwegayura
 
-chrome.app.runtime.addListener( function() {
+chrome.app.runtime.onLaunched.addListener( function() {
   chrome.app.window.create('login.html', {
     'bounds': {
-      'weight': 900,
-      'height': 900,
+      'width': 900,
+      'height': 900
     }
   });
 });
